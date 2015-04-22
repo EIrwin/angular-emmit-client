ccf167dd72de434b8f49223372b6efa46e8d417f
Eric Irwin <Eric.Irwin1124@gmail.com>
2015-04-22 10:36:43 -0700

    Updated createProxy(..) to use promises

--- emmit-client.js
+++ emmit-client.js
@@ -2,14 +2,25 @@ angular.module('EmmitClient',['SignalR']).
 service('Emmit',['$rootScope','Hub','$log','$timeout','$q',function($rootScope,Hub,$log,$timeout,$q){
 
    var createProxy = function(config){
-       var hub = new Hub(config.emitter,{
-           rootPath:config.path,
-           listeners:config.listeners,
-           queryParams:config.queryParams,
-           errorHandler:config.onError,
-           hubDisconnected:config.onDisconnected
-           });
-       return hub;
+   		var d = $q.defer();
+
+		try
+		{
+	        var hub = new Hub(config.emitter,{
+	           rootPath:config.path,
+	           listeners:config.listeners,
+	           queryParams:config.queryParams,
+	           errorHandler:config.onError,
+	           hubDisconnected:config.onDisconnected
+	           });
+
+	        d.resolve(hub);
+	    }
+	    catch(e){
+	    	d.reject(e);
+	    }
+
+        return d.promise;
    };
 
    return {
